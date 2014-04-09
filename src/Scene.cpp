4c0bced7f9ac38344ea18a620fc3d34966a83807
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2014-04-09 17:21:52 +0100

    Threads in progress v2.0
    WORKING

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -20,30 +20,38 @@ void Scene::init() {
 
 void Scene::draw() {
 	time_t timer1, timer2;
-	int nCPU = omp_get_num_procs();
-	omp_set_num_threads(nCPU);
-	std::cout << "CPU: " << nCPU << std::endl;
-	std::cout << "nthreads: " << omp_get_num_threads() << std::endl;
 	int RES_X = Camera::getInstance()->GetResX();
 	int RES_Y = Camera::getInstance()->GetResY(); 
-	
+	int TOTAL = RES_X * RES_Y;
+	int nCPU = omp_get_num_procs();
+	std::vector<RGB> image = std::vector<RGB>(TOTAL);
+
+	// Ray Tracing Calculation
 	time(&timer1);
-	//para cada pixel do viewport 
+	omp_set_num_threads(nCPU);
 	#pragma omp parallel for schedule(static, (RES_Y) / (nCPU * nCPU))	
 	for (int y = 0; y < RES_Y; y++) {
 		for (int x = 0; x < RES_X; x++) {
 			//determinar em WCS o raio primario que vai do centro de projecao ao pixel
 			Ray ray = Camera::getInstance()->PrimaryRay(x, y);
 			RGB color = RayTracer::getInstance()->trace(_background, _lights, _objects, ray, 1, 1.0f);
+			image[(RES_X*y) + x] = color;
+		}
+	}
+	time(&timer2);
+	float time = difftime(timer2, timer1);
+	std::cout << "Time spent Ray Tracing: " << time << " seconds" << std::endl;
+
+	// Draw Scene
+	for (int y = 0; y < RES_Y; y++) {
+		for (int x = 0; x < RES_X; x++) {
 			glBegin(GL_POINTS);		
-			glColor3f(color.r, color.g, color.b);
+			glColor3f(image[(RES_X*y) + x].r, image[(RES_X*y) + x].g, image[(RES_X*y) + x].b);
 			glVertex2f(x, y);
 			glEnd();			
 		}
 		glFlush();
-	}	
-	time(&timer2);
-	float time = difftime(timer2, timer1);
-	std::cout << "Time spent drawing: " << time << " seconds" << std::endl;
+	}
+
 	printf("Terminou!\n"); 
 }
