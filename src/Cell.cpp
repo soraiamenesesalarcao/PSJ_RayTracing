f4eaaf1977914cf481aa6e09cc944903c24d83ba
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2014-04-13 11:47:29 +0100

    Grid 0.0001
    Cell probably done

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -1,1 +1,22 @@
 #include "Cell.h"
+
+
+int Cell::getIndex() {
+	return _index;
+}
+
+
+void Cell::setIndex(int index) {
+	_index = index;
+}
+
+
+bool Cell::isEmpty(){
+	return _objects.empty();
+}
+
+
+void Cell::addObject(Object * object) {
+	_objects.push_back(object);
+}
+
--- src/Cell.h
+++ src/Cell.h
@@ -2,16 +2,18 @@
 
 #include "Definitions.h"
 #include "Object.h"
+#include "Sphere.h"
+#include "Polygon.h"
 
 class Cell {
 	
 	private:
 		int _index;
-		std::vector<Object> _objects;
+		std::vector<Object *> _objects;
 
 	public:
 		int getIndex();
-
 		void setIndex(int index);
 		bool isEmpty();
+		void addObject(Object * object);
 };
